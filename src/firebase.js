c8c5ac49a899236f036695457d0c72d9c3b594d8
nailgj.bnm@gmail.com <nabilgj.bnm@gmail.com>
2020-08-14 17:58:10 -0400

    added initial files for the project and cleanedup a lil bit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAs_7bPNvMpnhFFCa29nMqKSmB0TOsdDvw",
+  authDomain: "clone-91b13.firebaseapp.com",
+  databaseURL: "https://clone-91b13.firebaseio.com",
+  projectId: "clone-91b13",
+  storageBucket: "clone-91b13.appspot.com",
+  messagingSenderId: "316387018728",
+  appId: "1:316387018728:web:834f5cd0999911be16c65d",
+  measurementId: "G-T4S92YTHPL",
+};
